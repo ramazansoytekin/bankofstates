3cd05259bf55e5156e318f300fc7b1d91c0b605d
Ramazan Soytekin <soytekinramazan@gmail.com>
2020-12-20 18:15:28 +0100

    Product Component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StateProvider } from "./StateProvider";
+import reducer, { initialState } from "./reducer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.Fragment>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </React.Fragment>,
   document.getElementById('root')
 );
 
