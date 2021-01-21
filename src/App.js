9732b41ce46719f108e049f50721806e22a88e64
Ramazan Soytekin <soytekinramazan@gmail.com>
2021-01-22 00:12:55 +0100

    Register and Service

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './App.css';
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Home from "./home/Home";
+import Login from "./login/Login";
+import Register from "./register/Register";
 
 function App() {
   return (
@@ -10,8 +13,12 @@ function App() {
     <div>
       <Header/>
       <Switch>
-        <Route path="/login">Login</Route>
-        <Route path="/register">Register</Route>
+        <Route path="/login" >
+          <Login/>
+        </Route>
+        <Route path="/register">
+          <Register/>
+        </Route>
         <Route path="/" component={Home} />
       </Switch>
       <Footer/>
